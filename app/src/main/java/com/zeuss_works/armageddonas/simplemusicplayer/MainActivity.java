26039c8bbee226f164ba58160399451ebbf065eb
darknight <icsd11175@icsd.aegean.gr>
2015-12-15 18:54:39 +0200

    Attempt to create functional tests

--- app/src/main/java/com/zeuss_works/armageddonas/simplemusicplayer/MainActivity.java
+++ app/src/main/java/com/zeuss_works/armageddonas/simplemusicplayer/MainActivity.java
@@ -27,13 +27,15 @@ import android.support.v4.app.FragmentTransaction;
 /**
  * A simple launcher activity containing a summary sample description, sample log and a custom
  * {@link android.support.v4.app.Fragment} which can display a view.
- * <p>
+ * <p/>
  * For devices with displays with a width of 720dp or greater, the sample log is always visible,
  * on other devices it's visibility is controlled by an item on the Action Bar.
  */
 public class MainActivity extends FragmentActivity {
 
     public static final String TAG = "MainActivity";
+
+    private RecyclerViewFragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +44,14 @@ public class MainActivity extends FragmentActivity {
 
         if (savedInstanceState == null) {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            RecyclerViewFragment fragment = new RecyclerViewFragment();
+            fragment = new RecyclerViewFragment();
             transaction.replace(R.id.sample_content_fragment, fragment);
             transaction.commit();
         }
+    }
+
+    public RecyclerViewFragment getRecyclerViewFragment() {
+        return fragment;
     }
 
     //todo: remove comments
--- app/src/test/java/com/zeuss_works/armageddonas/simplemusicplayer/ExampleUnitTest.java
+++ app/src/test/java/com/zeuss_works/armageddonas/simplemusicplayer/ExampleUnitTest.java
@@ -3,22 +3,15 @@ package com.zeuss_works.armageddonas.simplemusicplayer;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import com.zeuss_works.armageddonas.simplemusicplayer.RecyclerViewFragment;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-public class ExampleUnitTest {
-    @Test
+public class ExampleUnitTest{
+    /*@Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        assertEquals(5, 2 + 2);
         assertNotNull(EmailValidator.isValidEmail("name@email.com"), is(true));
-    }
-
-    @Test
-    public void load_music_from_raw_works() {
-        ClassUnderTest myObjectUnderTest = new ClassUnderTest(CustomAdapter);
-        MusicFile songs[] = myObjectUnderTest.getObjectUnderTest.loadDataset();
-
-        assertNotNull(songs[0].title);
-    }
+    }*/
 }
